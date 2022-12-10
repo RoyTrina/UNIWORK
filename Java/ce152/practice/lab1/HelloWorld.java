d3f43f9ad1ae4e4b2019538e23905545bd47dba6
Tina <44895876+RoyTrina@users.noreply.github.com>
2022-12-10 17:49:34 +0000

    Add files via upload

---
+++ Java/ce152/practice/lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package practice.lab1;
+
+/**
+ * Created by tr17548 on 17/01/2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World, again!");
+    }
+}
---
+++ Java/ce152/practice/lab1/Main.java
@@ -0,0 +1,12 @@
+package practice.lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        //Lab.calcInterest();
+        Lab.calcMeans();
+        //Lab.greetName();
+        //Lab.escapeSeq();
+        //Lab.nextCharacters();
+        //Lab.ageDifference();
+    }
+}
